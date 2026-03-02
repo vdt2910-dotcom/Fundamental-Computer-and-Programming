884fb923e2d28a4ad14c9aca5d107cc33ab03062
vdt2910-dotcom <vdt2910@gmail.com>
2026-03-02 17:04:02 +0700

    Update 1.py

--- Assignments 4/1.py
+++ Assignments 4/1.py
@@ -1,9 +1,12 @@
-def course_codes():
-    clss = input("Enter your course code: ")
-
-    if clss[:2].isupper():
-        return True
-    else:
-        return False
-
-print(course_codes())
+def course_code():
+    
+        nhap_khoa_hoc = input(" Nhap khoa hoc vao cho bo: ")
+        
+        if nhap_khoa_hoc[:3].upper() and nhap_khoa_hoc[3:].isdigit() and nhap_khoa_hoc == 6:
+            return " True "
+        else :
+            return " False "
+    
+    
+print(course_code())
+            
