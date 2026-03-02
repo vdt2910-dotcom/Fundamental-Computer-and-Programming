e4b1728f5a6a2ce0181e49326e23d3a3b35dd6f6
vdt2910-dotcom <vdt2910@gmail.com>
2026-03-02 19:39:50 +0700

    Update 4.py

--- Assignments 4/4.py
+++ Assignments 4/4.py
@@ -1,14 +1,9 @@
-phone_number_list = []
+def phone_protect():
+    while True :
+        sdt = input(" Pls enteer your phone number: ")
+        if sdt.startswith("+84") or sdt.startswith("0"):
+            anonymous = "#" * (len(sdt)-3) + sdt[7:] 
+            return anonymous
 
-while True:
-    phone = input("Type your phone number here: ")
 
-    if phone.startswith("0") or phone.startswith("+84"):
-        phone_number_list.append(phone)
-        
-        anonymous = "#" * (len(phone) - 3) + phone[-3:]
-        print("Your phone number is anonymized:", anonymous)
-        break
-    else:
-        print("Try again")
-        
+print(phone_protect())
