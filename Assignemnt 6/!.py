64f5a4be4dabce4df672f3e98822d5c61aaffc59
vdt2910-dotcom <vdt2910@gmail.com>
2026-03-08 07:12:13 +0700

    Add files via upload

---
+++ Assignemnt 6/!.py
@@ -0,0 +1,35 @@
+numbers_list = []
+while True:
+     number = input("Hay nhap so")
+     if number =="" :
+        break
+     if number.isdigit():
+            numbers_list.append(number)
+            numbers_list.sort(reverse=True)
+print(numbers_list[:5])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+ 
+ 
+                
+           
+
+   
+
+
+   
+   
+
---
+++ Assignemnt 6/2.py
@@ -0,0 +1,9 @@
+month = int(input("Enter the month"))
+if 3<= month <=5:
+       print(" it is spring")
+elif 6<= month <=8:
+       print(" it is summer")
+elif 9<= month <=11:
+       print(" it is autumn")
+elif month == 12 or month == 1 or month == 2:
+       print(" it is winter")
